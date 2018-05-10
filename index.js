ed59de8c4e5dce3690a46878555800cdb720651d
AlanJ <s00168420@mail.itsligo.ie>
2018-05-10 19:28:13 +0100

    added missing |

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express'),
     app = express(),
-    port = process.env.PORT | 8080
+    port = process.env.PORT || 8080
 
 app.use(express.static('/public'))
 app.set('view-engine', 'pug')
